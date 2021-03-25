bfc34a7e8895eb081bee356aff9d4016f6fa6bf6
Enkada <65423212+Enkada@users.noreply.github.com>
2021-03-25 10:07:50 +0300

    Update Source.cpp

--- BankAccountApp/Source.cpp
+++ BankAccountApp/Source.cpp
@@ -14,7 +14,7 @@ struct Account {
 	time_t lastChange;
 };
 
-string types[]{ "срочный", "льготный", "брокерский", "бюджетный", "накопительный", "лицевой", "инвестиционный" };
+string types[]{ "Г±Г°Г®Г·Г­Г»Г©", "Г«ГјГЈГ®ГІГ­Г»Г©", "ГЎГ°Г®ГЄГҐГ°Г±ГЄГЁГ©", "ГЎГѕГ¤Г¦ГҐГІГ­Г»Г©", "Г­Г ГЄГ®ГЇГЁГІГҐГ«ГјГ­Г»Г©", "Г«ГЁГ¶ГҐГўГ®Г©", "ГЁГ­ГўГҐГ±ГІГЁГ¶ГЁГ®Г­Г­Г»Г©" };
 
 int accountNumber = 0;
 int currentSize = 0;
@@ -48,24 +48,24 @@ void deleteClient(int number) {
 }
 
 void getClientInfo(int number) {
-	cout << "[Счет №" << clients[number].number << "]:" << endl;
-	cout << "\tФИО: " << clients[number].fullName << endl;
-	cout << "\tТип счета: " << types[clients[number].type] << endl;
-	cout << "\tБаланс счета: " << fixed << setprecision(2) << clients[number].balance << " рублей" << endl;
-	cout << "\tПоследнее изменение: " << ctime(&clients[number].lastChange);
+	cout << "[Г‘Г·ГҐГІ В№" << clients[number].number << "]:" << endl;
+	cout << "\tГ”Г€ГЋ: " << clients[number].fullName << endl;
+	cout << "\tГ’ГЁГЇ Г±Г·ГҐГІГ : " << types[clients[number].type] << endl;
+	cout << "\tГЃГ Г«Г Г­Г± Г±Г·ГҐГІГ : " << fixed << setprecision(2) << clients[number].balance << " Г°ГіГЎГ«ГҐГ©" << endl;
+	cout << "\tГЏГ®Г±Г«ГҐГ¤Г­ГҐГҐ ГЁГ§Г¬ГҐГ­ГҐГ­ГЁГҐ: " << ctime(&clients[number].lastChange);
 
 }
 
 void main() {
 	setlocale(LC_ALL, "Russian");
-	cout << "Лабораторная работа по теме Структуры | Хорев Кирилл гр.219/4" << endl << endl;
+	cout << "Г‹Г ГЎГ®Г°Г ГІГ®Г°Г­Г Гї Г°Г ГЎГ®ГІГ  ГЇГ® ГІГҐГ¬ГҐ Г‘ГІГ°ГіГЄГІГіГ°Г» | Г•Г®Г°ГҐГў ГЉГЁГ°ГЁГ«Г« ГЈГ°.219/4" << endl << endl;
 
-	string firstNames[10]{ "Михаил", "Александр",  "Константин",  "Петр",  "Дмитрий",  "Владимир",  "Кирилл",  "Егор",  "Борис",  "Семён", };
-	string lastNames[10]{ "Петров", "Сидоров",  "Лебедев",  "Соколов",  "Виноградов",  "Иванов",  "Вавилов",  "Смирнов",  "Павлов",  "Морозов", };
+	string firstNames[10]{ "ГЊГЁГµГ ГЁГ«", "ГЂГ«ГҐГЄГ±Г Г­Г¤Г°",  "ГЉГ®Г­Г±ГІГ Г­ГІГЁГ­",  "ГЏГҐГІГ°",  "Г„Г¬ГЁГІГ°ГЁГ©",  "Г‚Г«Г Г¤ГЁГ¬ГЁГ°",  "ГЉГЁГ°ГЁГ«Г«",  "Г…ГЈГ®Г°",  "ГЃГ®Г°ГЁГ±",  "Г‘ГҐГ¬ВёГ­", };
+	string lastNames[10]{ "ГЏГҐГІГ°Г®Гў", "Г‘ГЁГ¤Г®Г°Г®Гў",  "Г‹ГҐГЎГҐГ¤ГҐГў",  "Г‘Г®ГЄГ®Г«Г®Гў",  "Г‚ГЁГ­Г®ГЈГ°Г Г¤Г®Гў",  "Г€ГўГ Г­Г®Гў",  "Г‚Г ГўГЁГ«Г®Гў",  "Г‘Г¬ГЁГ°Г­Г®Гў",  "ГЏГ ГўГ«Г®Гў",  "ГЊГ®Г°Г®Г§Г®Гў", };
 	for (int i = 0; i < 10; i++) {
 		string name = firstNames[rand() % 10] + " " + lastNames[rand() % 10];
 		addClient(name, rand() % 4);
-		clients[i].balance = (rand() % 50000) + (double)(rand() % 99) / 100;
+		clients[i].balance = (rand() % 55000) + (double)(rand() % 99) / 100;
 	}
 
 	int select = 0;
@@ -73,20 +73,20 @@ void main() {
 	string inputFullName = ""; int inputType = 0; int inputNumber = 0;
 	float inputRange1; float inputRange2;
 	do {
-		cout << "[Меню действий]:" << endl;
-		cout << "\t1) Добавить нового клиента" << endl;
-		cout << "\t2) Получить информацию о всех клиентах" << endl;
-		cout << "\t3) Удаление клиентов" << endl;
-		cout << "\t4) Поиск клиентов" << endl;
-		cout << "\t5) Выход из программы" << endl;
+		cout << "[ГЊГҐГ­Гѕ Г¤ГҐГ©Г±ГІГўГЁГ©]:" << endl;
+		cout << "\t1) Г„Г®ГЎГ ГўГЁГІГј Г­Г®ГўГ®ГЈГ® ГЄГ«ГЁГҐГ­ГІГ " << endl;
+		cout << "\t2) ГЏГ®Г«ГіГ·ГЁГІГј ГЁГ­ГґГ®Г°Г¬Г Г¶ГЁГѕ Г® ГўГ±ГҐГµ ГЄГ«ГЁГҐГ­ГІГ Гµ" << endl;
+		cout << "\t3) Г“Г¤Г Г«ГҐГ­ГЁГҐ ГЄГ«ГЁГҐГ­ГІГ®Гў" << endl;
+		cout << "\t4) ГЏГ®ГЁГ±ГЄ ГЄГ«ГЁГҐГ­ГІГ®Гў" << endl;
+		cout << "\t5) Г‚Г»ГµГ®Г¤ ГЁГ§ ГЇГ°Г®ГЈГ°Г Г¬Г¬Г»" << endl;
 
-		cout << "Выберите действие - "; cin >> select; cout << endl;
+		cout << "Г‚Г»ГЎГҐГ°ГЁГІГҐ Г¤ГҐГ©Г±ГІГўГЁГҐ - "; cin >> select; cout << endl;
 
 		switch (select) {
 		case 1:
-			cout << "Введите ФИО клиента - ";
+			cout << "Г‚ГўГҐГ¤ГЁГІГҐ Г”Г€ГЋ ГЄГ«ГЁГҐГ­ГІГ  - ";
 			cin >> inputFullName;
-			cout << "Введите тип счета (";
+			cout << "Г‚ГўГҐГ¤ГЁГІГҐ ГІГЁГЇ Г±Г·ГҐГІГ  (";
 			for (int i = 0; i < (sizeof(types) / sizeof(string)); i++) {
 				cout << i << " - " << types[i];
 				if ((i + 1) == (sizeof(types) / sizeof(string))) {
@@ -105,14 +105,14 @@ void main() {
 			}
 			break;
 		case 3:
-			cout << "[Удаление клиентов]:" << endl;
-			cout << "\t1) Удаление клиента по номеру счета" << endl;
-			cout << "\t2) Удаление всех клиентов" << endl;
-			cout << "Выберите действие - "; cin >> select; cout << endl;
+			cout << "[Г“Г¤Г Г«ГҐГ­ГЁГҐ ГЄГ«ГЁГҐГ­ГІГ®Гў]:" << endl;
+			cout << "\t1) Г“Г¤Г Г«ГҐГ­ГЁГҐ ГЄГ«ГЁГҐГ­ГІГ  ГЇГ® Г­Г®Г¬ГҐГ°Гі Г±Г·ГҐГІГ " << endl;
+			cout << "\t2) Г“Г¤Г Г«ГҐГ­ГЁГҐ ГўГ±ГҐГµ ГЄГ«ГЁГҐГ­ГІГ®Гў" << endl;
+			cout << "Г‚Г»ГЎГҐГ°ГЁГІГҐ Г¤ГҐГ©Г±ГІГўГЁГҐ - "; cin >> select; cout << endl;
 
 			switch (select) {
 			case 1:
-				cout << "Введите № счета клиента - "; cin >> inputNumber;
+				cout << "Г‚ГўГҐГ¤ГЁГІГҐ В№ Г±Г·ГҐГІГ  ГЄГ«ГЁГҐГ­ГІГ  - "; cin >> inputNumber;
 				deleteClient(inputNumber);
 				break;
 			case 2:
@@ -122,18 +122,18 @@ void main() {
 				}
 				break;
 			default:
-				cout << "Значение должно быть в диапазоне 1-2" << endl;
+				cout << "Г‡Г­Г Г·ГҐГ­ГЁГҐ Г¤Г®Г«Г¦Г­Г® ГЎГ»ГІГј Гў Г¤ГЁГ ГЇГ Г§Г®Г­ГҐ 1-2" << endl;
 			}
 			break;
 		case 4:
-			cout << "[Поиск клиентов]" << endl;
-			cout << "\t1) По номеру счета" << endl;
-			cout << "\t2) По диапазону суммы" << endl;
-			cout << "Выберите действие - "; cin >> select; cout << endl;
+			cout << "[ГЏГ®ГЁГ±ГЄ ГЄГ«ГЁГҐГ­ГІГ®Гў]" << endl;
+			cout << "\t1) ГЏГ® Г­Г®Г¬ГҐГ°Гі Г±Г·ГҐГІГ " << endl;
+			cout << "\t2) ГЏГ® Г¤ГЁГ ГЇГ Г§Г®Г­Гі Г±ГіГ¬Г¬Г»" << endl;
+			cout << "Г‚Г»ГЎГҐГ°ГЁГІГҐ Г¤ГҐГ©Г±ГІГўГЁГҐ - "; cin >> select; cout << endl;
 
 			switch (select) {
 			case 1:
-				cout << "Введите номер счета клиента - "; cin >> inputNumber;
+				cout << "Г‚ГўГҐГ¤ГЁГІГҐ Г­Г®Г¬ГҐГ° Г±Г·ГҐГІГ  ГЄГ«ГЁГҐГ­ГІГ  - "; cin >> inputNumber;
 				for (int i = 0; i < currentSize; i++) {
 					if (clients[i].number == inputNumber) {
 						getClientInfo(i);
@@ -141,24 +141,24 @@ void main() {
 				}
 				break;
 			case 2:
-				cout << "Введите первое число диапазона - "; cin >> inputRange1;
-				cout << "Введите второе число диапазона - "; cin >> inputRange2;
+				cout << "Г‚ГўГҐГ¤ГЁГІГҐ ГЇГҐГ°ГўГ®ГҐ Г·ГЁГ±Г«Г® Г¤ГЁГ ГЇГ Г§Г®Г­Г  - "; cin >> inputRange1;
+				cout << "Г‚ГўГҐГ¤ГЁГІГҐ ГўГІГ®Г°Г®ГҐ Г·ГЁГ±Г«Г® Г¤ГЁГ ГЇГ Г§Г®Г­Г  - "; cin >> inputRange2;
 
-				cout << "[Счета клиентов с балансом в диапазоне (" << inputRange1 << " - " << inputRange2 << ")]:" << endl;
+				cout << "[Г‘Г·ГҐГІГ  ГЄГ«ГЁГҐГ­ГІГ®Гў Г± ГЎГ Г«Г Г­Г±Г®Г¬ Гў Г¤ГЁГ ГЇГ Г§Г®Г­ГҐ (" << inputRange1 << " - " << inputRange2 << ")]:" << endl;
 				for (int i = 0; i < currentSize; i++) {
 					if (clients[i].balance >= inputRange1 && clients[i].balance <= inputRange2) {
-						cout << "\t(Счет №" << clients[i].number << ")" << " Баланс счета: " << fixed << setprecision(2) << clients[i].balance << " рублей" << endl;
+						cout << "\t(Г‘Г·ГҐГІ В№" << clients[i].number << ")" << " ГЃГ Г«Г Г­Г± Г±Г·ГҐГІГ : " << fixed << setprecision(2) << clients[i].balance << " Г°ГіГЎГ«ГҐГ©" << endl;
 					}
 				}
 				break;
 			default:
-				cout << "Значение должно быть в диапазоне 1-5" << endl;
+				cout << "Г‡Г­Г Г·ГҐГ­ГЁГҐ Г¤Г®Г«Г¦Г­Г® ГЎГ»ГІГј Гў Г¤ГЁГ ГЇГ Г§Г®Г­ГҐ 1-5" << endl;
 			}
 			break;
 		case 5:
 			break;
 		default:
-			cout << "Значение должно быть в диапазоне 1-5" << endl;
+			cout << "Г‡Г­Г Г·ГҐГ­ГЁГҐ Г¤Г®Г«Г¦Г­Г® ГЎГ»ГІГј Гў Г¤ГЁГ ГЇГ Г§Г®Г­ГҐ 1-5" << endl;
 		}
 		cout << endl; system("pause"); cout << endl;
 	} while (select != 5);
